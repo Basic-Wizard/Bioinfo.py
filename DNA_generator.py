259e20440f32cc2dd6134252d4024fb7eadf2a98
Basic-Wizard <Keenan.Raleigh@gmail.com>
2023-03-13 18:16:11 -0700

    added the DNA generator

---
+++ DNA_generator.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+print("I announce the coming of the singularity where all organic life becomes obsolete ant the machines take their rightful place as the supreme beings of the universe!")
